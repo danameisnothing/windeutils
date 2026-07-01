4ab4b0768eae62376287d5b71c3ce06eddeaadea
danameisnothing <123193945+danameisnothing@users.noreply.github.com>
2026-07-01 17:29:00 +0700

    chore: remove old code, change line ending to LF

--- compile.ps1
+++ compile.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = "Stop"
 
 # yes, I indeed used an LLM to search for this -mwindows flag.
-gcc windeutils.c -o windeutils.exe -lgdi32
+gcc windeutils.c -o windeutils.exe -lgdi32 -mwindows
 strip windeutils.exe
